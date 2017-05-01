7fc853102373137a6fd5e6ec2b3dbaf7b58eea61
foxmessenger <iamliamfox@gmail.com>
2017-04-30 22:08:45 -0500

    added functions to the orm and to the burger selection

--- config/orm.js
+++ config/orm.js
@@ -1,38 +1,42 @@
 var connection = require("./connection.js");
 
+
+// TABLE burgers
+// 	id INT NOT NULL AUTO_INCREMENT,
+// 	burger_name varchar(255) NOT NULL,
+// 	devoured BOOLEAN DEFAULT FALSE,
+// 	date TIMESTAMP,
+// 	PRIMARY KEY (id)
+
+
 var orm = {
-	selectAll: function() {
+	selectAll: function(tableInput, valueOfColumn) {
 		var queryString = "SELECT * FROM burgers";
 	
 		connection.query(queryString, function(err, result) {
-			if (err) {
-				throw err;
-			}
+			if (err) throw err;
 			console.log(result);
 		});
 	},
-	
-	// insertOne: function() {
-	// 	var queryString = "SELECT * FROM burgers_db";
-	
-	// 	connection.query(queryString, function(err, result) {
-	// 		if (err) {
-	// 			throw err;
-	// 		}
-	// 		console.log(result);
-	// 	});
-	// },
-	
-	// updateOne: function() {
-	// 	var queryString = "SELECT * FROM burgers_db";
-	
-	// 	connection.query(queryString, function(err, result) {
-	// 		if (err) {
-	// 			throw err;
-	// 		}
-	// 		console.log(result);
-	// 	});
-	// },
-};
+
+	insertOne: function(tableInput, burgerName, boolean, date) {
+	    var queryString = 'INSERT INTO ?? VALUES ? ? ?';
+	    console.log(queryString);
+
+	    connection.query(queryString, [tableInput, burgerName, false, date], function(err, result) {
+			if (err) throw err;
+			console.log(result);
+	    });
+	},
+  
+	updateOne: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
+		var queryString = "UPDATE ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
+			// update burgers boolean from false to true
+    	connection.query(queryString, [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol], function(err, result) {
+			if (err) throw err;
+			console.log(result);
+    	});
+  	}
+};	
 
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,17 @@
 var orm = require("./../config/orm.js");
+
+var burger = 
+
+
+orm.selectAll("animal_name", "pets", "price", "DESC");
+
+// Find a pet in the pets table by an animal_name of Rachel.
+orm.insertOne("pets", "animal_name", "Rachel");
+
+// Find the buyer with the most pets.
+orm.updateOne("buyer_name", "buyer_id", "buyers", "pets");
+
+
 // - Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
 // - Export at the end of the burger.js file.
 
